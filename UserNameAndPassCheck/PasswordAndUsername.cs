274f9d998a96b120496aff47452194cb97817637
PC <nikoloz.de@gmail.com>
2023-04-21 18:34:21 +0400

    Add project files.

---
+++ UserNameAndPassCheck/PasswordAndUsername.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserNameAndPassCheck
+{
+    public class PasswordAndUsername
+    {
+        public string InputPassword { get; set; } = string.Empty;
+        public string InputUsername { get; set; } = string.Empty;
+
+
+        private void UserInput()
+        {
+            Console.Write("Please input the Username: ");
+            InputUsername = Console.ReadLine();
+            Console.Write("Please input the Password: ");
+            InputPassword = Console.ReadLine();
+        }
+
+        public void UsernameAndPasswordCheck(string username, string password)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                UserInput();
+
+                if (InputPassword.Equals(password) && InputUsername.Equals(username))
+                {
+                    Console.WriteLine("Well Done!");
+                    break;
+                }
+                else
+                {
+                    if (i == 4)
+                        Console.WriteLine("Error! Invalid input!");
+                    else 
+                        Console.WriteLine("try again!");
+                }
+            }
+        }
+    }
+}
---
+++ UserNameAndPassCheck/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UserNameAndPassCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordAndUsername passwordAndUsername = new PasswordAndUsername();    
+            string userName = "nick";
+            string passwprd = "123Abc";
+
+            Console.WriteLine("Welcome to my Project!");
+            passwordAndUsername.UsernameAndPasswordCheck(userName, passwprd);
+            Console.ReadKey();
+        }
+    }
+}
